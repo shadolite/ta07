fc48fb2807c6f9e0fbc3be7b570ccadfed5735e3
shadolite <amy.kawaii@gmail.com>
2017-05-30 21:52:15 -0600

    ta07

---
+++ ta07.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Core requirements
+float getValueFromPointer(float* thePointer);
+float* getMinValue(float* a, float* b);
+
+// Stretch goals
+void swapElements(float* theArray[], int a, int b);
+void sortArray(float* theArray[]);
+
+int main()
+{  
+   /* // Core Requirement 1
+   int arraySize;
+   cout << "Enter the array size: ";
+   cin >> arraySize;
+
+   // Allocate your array(s) here
+
+   // Fill your array with float values
+   for(int i = 0; i < arraySize; i++) 
+   {
+          cout << "Enter a float value: ";
+          cin >> ????
+   }*/
+
+   /* // Core Requirement 2
+   for (int i = 0; i < arraySize; i++)
+   {
+      float value = getValueFromPointer(????);
+      cout << "The value of the element " << i << " is: ";
+      cout << value << endl;
+   }
+   */
+
+   /* // Core Requirement 3
+   // Print the smaller of the first and last elements of the array
+   float *pointerToMin = getMinValue(????, ????);
+   cout << ?????
+   */
+
+   // Clean up your array(s) here
+
+   return 0;
+}
+
